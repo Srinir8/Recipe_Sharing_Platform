f8ee5addefa9ebb3d934e974349471a3d7f7ccee
Srini R <srinir8@deloitte.com>
2025-05-18 18:13:59 +0530

    updated url

--- Frontend/RECIPE-SHARING-PLATFORM/src/environments/environment.ts
+++ Frontend/RECIPE-SHARING-PLATFORM/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44309/api',
-  Url:'https://localhost:44309' // Base URL for the API
+apiUrl: 'https://recipe-sharing-platform-fdim.onrender.com/api',
+  Url:'https://recipe-sharing-platform-fdim.onrender.com' // Base URL for the API
 };
